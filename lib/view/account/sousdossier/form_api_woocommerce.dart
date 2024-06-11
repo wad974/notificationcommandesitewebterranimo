26f2f336624613025d873eb888a308a5da5a200f
wad974 <woodartdeco974@gmai.com>
2024-06-11 06:26:18 +0400

    amelioration code accountpage

--- lib/view/account/sousdossier/form_api_woocommerce.dart
+++ lib/view/account/sousdossier/form_api_woocommerce.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
-import 'package:notification_app_woocommerce/model/config_param.dart';
 
 class FormApiWooCommerce extends StatelessWidget {
   String urlApiWooCommerce;
